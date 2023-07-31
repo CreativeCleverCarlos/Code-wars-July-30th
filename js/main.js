4f26b729b282f7b2bed5607dc72baca0abf6dd64
Carlos Campbell <carlos.g.campbell@gmail.com>
2023-07-30 22:47:01 -0400

    full code war

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+**/
+
+function oddOrEven(array) {
+   let sumOfArray = array.reduce((a,b)=> a + b, 0)
+   if (sumOfArray == 0){
+    return "even"
+   } else if (sumOfArray % 2 == 0){
+    return "even"
+   } else{
+    return "odd"
+   }
+}
+
+console.log(oddOrEven([1,2]))
